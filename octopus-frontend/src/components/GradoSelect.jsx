30679d8b30dc1fa12d9980f1cfccf242485b57c5
jfgoitiac <jfgoitiac@gmail.com>
2026-07-08 11:38:58 -0400

    inscripcion harcodeada

--- octopus-frontend/src/components/GradoSelect.jsx
+++ octopus-frontend/src/components/GradoSelect.jsx
@@ -1,15 +1,28 @@
-import { GRADOS_PRIMARIA, GRADOS_MEDIA } from '../constants/grados';
+import { useEffect, useState } from 'react';
+import axiosInstance from '../api/apiClient';
 
-const GradoSelect = ({ value, onChange, className, style, incluirVacio = false }) => (
-  <select value={value || ''} onChange={onChange} className={className} style={style}>
-    {incluirVacio && <option value="">Seleccionar grado...</option>}
-    <optgroup label="Primaria">
-      {GRADOS_PRIMARIA.map(g => <option key={g} value={g}>{g}</option>)}
-    </optgroup>
-    <optgroup label="Media General">
-      {GRADOS_MEDIA.map(g => <option key={g} value={g}>{g}</option>)}
-    </optgroup>
-  </select>
-);
+const GradoSelect = ({ value, onChange, className, style, incluirVacio = false, ...rest }) => {
+  const [grados, setGrados] = useState([]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    axiosInstance
+      .get('secretaria/configuracion-grados/', { signal: controller.signal })
+      .then(res => setGrados(res?.data || []))
+      .catch(err => {
+        if (err.name !== 'CanceledError') setGrados([]);
+      });
+    return () => controller.abort();
+  }, []);
+
+  return (
+    <select value={value || ''} onChange={onChange} className={className} style={style} {...rest}>
+      {incluirVacio && <option value="">Seleccionar grado...</option>}
+      {grados.map(g => (
+        <option key={g.id} value={g.grado_seccion}>{g.grado_seccion}</option>
+      ))}
+    </select>
+  );
+};
 
 export default GradoSelect;
--- octopus-frontend/src/constants/grados.jsx
+++
@@ -1,9 +0,0 @@
-export const GRADOS_PRIMARIA = [
-  '1er Grado', '2do Grado', '3er Grado', '4to Grado', '5to Grado', '6to Grado',
-];
-
-export const GRADOS_MEDIA = [
-  '1er Año', '2do Año', '3er Año', '4to Año', '5to Año',
-];
-
-export const TODOS_LOS_GRADOS = [...GRADOS_PRIMARIA, ...GRADOS_MEDIA];
